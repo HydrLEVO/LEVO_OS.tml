1b1aa40391fa47c3b83469af15e167c99a2297aa
LEVO <90807226+HydrLEVO@users.noreply.github.com>
2021-09-18 18:33:28 +1000

    Add files via upload

---
+++ LEVO.py
@@ -0,0 +1,346 @@
+import time
+import os
+import socket
+
+print("""
+                                                                  LEVO_GRUB
+                                            ___________________________________________________________
+                                            |  [1] LEVO_BOOTUP​​​​​                                         |
+                                            |  [2] LEVO_RECOVERING_SYSTEM                              |
+                                            |  [4] DATA_STORAGE                                        |
+                                            |                                                          | 
+                                            |________________________LEVO_BETA_________________________|
+""")
+select = input("[?]: ")
+
+if select == '1':
+    print("""
+                                                              LEVO
+""")
+print("""
+                                                        [1] To Open VIRTAUL-BOX
+                                                        [2] free Text
+                                                        [3] To Open File Exploer
+                                                        [4] storage_system
+                                                        [5] To Open Terminal
+                                                        [6] To Open Calculator
+                                                        
+                                                        [7] hydra_echo
+                                                        
+                                                        
+                                                        [q] To Close OS Safley
+""")
+
+select = input(">: ")
+if select == 'q':
+    os.system('exit')
+
+if select == '1':
+    print("""
+    [1] LEVO_ALPHA
+    [2] LEVO_BETA2.1
+    [3] LEVO_LEPARD
+    [4] LEVO_dos
+    """)
+select = input(">: ")
+if select == '2':
+        print("please press 'f' to start LEVO_BETA2.1")
+        select = input (">: ")
+        
+if select == 'f':
+    print("""
+                                                          LEVO_BETA  2.1
+    """)
+    print("""
+                                                     [1] LEVO_virtaul-box
+                                                     [2] LEVO_user-account
+                                                     [3] LEVO_storage-system
+                                                     [4] AI_hydra-echo
+    
+                                                     [q] QUIT
+""")
+select = input(">: ")
+if select == '1':
+    print("""
+    [1] LEVO_ALPHA
+    [2] LEVO_BETA2.1
+    [3] LEVO_LEPARD
+    [4] LEVO_dos
+    """)
+select = input(">: ")
+    
+
+if select == '1':
+	    
+	print("please press 'S' to start LEVO_ALPHA.os")
+	select = input(">: ")
+	
+if select == 's':
+    print("""
+                                                             LEVO_ALPHA
+           """)
+print("""
+                                                        [1] To Open VIRTAUL-BOX
+                                                        [2] To Open Text Editor
+                                                        [3] storage_system
+                                                        [4] To Open Terminal
+                                                        
+                                                        [7] hydra_echo
+                                                       
+                                                       
+                                                       
+                                                       [q] To Close OS Safley
+           """)
+           
+select = input(">: ")
+
+if select == '2':
+    write = input(str(""))
+
+if select == '3':
+	print("""
+	Sorry, Cannot progress File system.
+	""")
+
+if select == '4':
+    print("""
+    LEVO_STORAGE-SYSTEM
+    
+    (C/_[USER_])>
+    Storage-System == 34.00_GIGS
+    package = C/_
+    user_account = LEVO.os
+    
+    INSTALLATION_LEVO.os
+    installation>>
+    
+    _________________________________________________________________________________________________________
+    █████████████████████████████████████████████████████████████████████████████████████████████████████████
+    
+    installation_LEVO.os = satisfied
+    lev-installation = 100%
+    
+    installation == Compleated
+    """)
+    
+if select == '5':
+    print("""
+    Terminal_lev-Hydra
+    
+    [C/_(USER)]>>
+    >>  You have only got a (BETA-RELEASE) terminal doesnt exist yet.
+    """)
+
+
+if select == 'q':
+    os.system('exit')
+
+if select == '2':
+    write = input(str(""))
+
+if select == '2':
+	freeWrite = input(str(""))
+
+
+if select == '3':
+	print("""
+	Sorry, Cannot progress File system.
+	""")
+
+if select == '4':
+    print("""
+    LEVO_STORAGE-SYSTEM
+    
+    (C/_[USER_])>
+    Storage-System == 34.00_GIGS
+    package = C/_
+    user_account = LEVO.os
+    
+    INSTALLATION_LEVO.os
+    installation>>
+    
+    _________________________________________________________________________________________________________
+    █████████████████████████████████████████████████████████████████████████████████████████████████████████
+    
+    installation_LEVO.os = satisfied
+    lev-installation = 100%
+    
+    installation == Compleated
+    """)
+    
+if select == '5':
+    print("""
+    Terminal_lev-Hydra
+    
+    [C/_(USER)]>>
+    >>  You have only got a (BETA-RELEASE) terminal doesnt exist yet.
+    """)
+
+
+if select == 'q':
+    os.system('exit')
+
+
+
+
+if select == '1':
+	select = input("[?]: ")
+	print("please press 'S' to start LEVO_ALPHA.os")
+	select = input("[?]: ")
+	
+	if select == 's':
+	
+           print("""
+                                                             LEVO_ALPHA
+           """)
+           print("""
+                                                        [1] To Open VIRTAUL-BOX
+                                                        [2] To Open Text Editor
+                                                        [3] storage_system
+                                                        [4] To Open Terminal
+                                                        
+                                                        [7] hydra_echo
+                                                       
+                                                       
+                                                       
+                                                       [q] To Close OS Safley
+""")
+select = input(">: ")
+          
+if select == '2':
+    write = input(str(""))
+
+if select == '2':
+	freeWrite = input(str(""))
+
+
+if select == '3':
+	print("""
+	Sorry, Cannot progress File system.
+	""")
+
+if select == '4':
+    print("""
+    LEVO_STORAGE-SYSTEM
+    
+    (C/_[USER_])>
+    Storage-System == 34.00_GIGS
+    package = C/_
+    user_account = LEVO.os
+    
+    INSTALLATION_LEVO.os
+    installation>>
+    
+    _________________________________________________________________________________________________________
+    █████████████████████████████████████████████████████████████████████████████████████████████████████████
+    
+    installation_LEVO.os = satisfied
+    lev-installation = 100%
+    
+    installation == Compleated
+    """)
+    
+if select == '5':
+    print("""
+    Terminal_lev-Hydra
+    
+    [C/_(USER)]>>
+    >>  You have only got a (BETA-RELEASE) terminal doesnt exist yet.
+    """)
+
+
+if select == 'q':
+    os.system("exit")
+
+
+    write = input(str(""))
+	  
+
+         
+	 
+
+if select == '3':
+   print("""
+                 LEVO_STORAGE-SYSTEM
+      
+                 (C/_[USER_])>
+                 Storage-System == 34.00_GIGS
+                 package = C/_
+                 user_account = LEVO.os
+    
+                 INSTALLATION_LEVO.os
+                 installation>>
+    
+               _________________________________________________________________________________________________________
+               █████████████████████████████████████████████████████████████████████████████████████████████████████████
+    
+                 installation_LEVO.os = satisfied
+                 lev-installation = 100%
+    
+                 installation == Compleated
+""")
+    
+if select == '4':
+    print("""
+                    Terminal_lev-Hydra
+    
+                    [C/_(USER)]>>
+                    >>  You have only got a (BETA-RELEASE) terminal doesnt exist yet.
+""")
+
+
+if select == 'q':
+	             os.system('exit')
+
+if select == '2':
+	os.startfile('cyber.py')
+	os.startfile('edit.pyw')
+
+if select == '3':
+    print("""
+    FILES CANNOT BE SHOWED DUE TO (BETA-RELEASE)
+""")
+
+if select == '4':
+    print("""
+    LEVO_STORAGE-SYSTEM
+    
+    (C/_[USER_])>
+    Storage-System == 34.00_GIGS
+    package = C/_
+    user_account = LEVO.os
+    
+    INSTALLATION_LEVO.os
+    installation>>
+    
+    _________________________________________________________________________________________________________
+    █████████████████████████████████████████████████████████████████████████████████████████████████████████
+    
+    installation_LEVO.os = satisfied
+    lev-installation = 100%
+    
+    installation == Compleated
+    """)
+    
+if select == '5':
+    print("""
+    Terminal_lev-Hydra
+    
+    [C/_(USER)]>>
+    >>  You have only got a (BETA-RELEASE) terminal doesnt exist yet.
+    """)
+
+
+if select == 'q':
+	os.system('exit')
+    
+if select == '2':
+    print("""
+    [1] LEVO_Core16.REC
+    [2] LEVO_Core17.REC
+    [3] LEVO_Recovery-System.REC
+    [4] back
+""")
+select = input(">: ")
+if select == '4':
+    os.system('exit')
